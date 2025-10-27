3587028fd66603a8467fa6cad01d275177a969a9
gabriel Santos <gabriel.s.silva95@aluno.senai.br>
2025-10-27 14:29:41 -0300

    rota principal feita

--- main.py
+++ main.py
@@ -12,3 +12,18 @@ templates = Jinja2Templates(directory="templates")
 #Definir a pasta onde está os arquivos estaticos (CSS, Imagens e JS)
 app.mount("/static", StaticFiles(directory="static"),
 name="static")
+
+alunos = [
+    {"nome": "Iago", "nota":8.5},
+    {"nome": "Murilo", "nota":6.5},
+    {"nome": "Joana", "nota":9.5},
+    {"nome": "Francisco", "nota":0.5},
+]
+
+#Rota principal
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "lista_alunos": alunos}
+    )
